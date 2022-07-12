ea2ade47b9df2956412973f4862143d6bec4deeb
Luis Antónimo <luisantonimo@gmail.com>
2022-07-12 09:02:35 +0200

    ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace T04Ejercicio16
+{
+    class Program
+    {
+        const int iva = 21;
+
+        static void Main()
+        {
+            Console.WriteLine("Escriba el precio de un producto:");
+            double numero = Convert.ToDouble(Console.ReadLine());
+            double precioFinal = numero + ((numero / 100) * iva);
+            Console.WriteLine("El precio + IVA es: " + precioFinal);
+        }
+    }
+}
